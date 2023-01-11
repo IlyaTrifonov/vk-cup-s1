3ad43f6cf1ddf69329ccc0caf02293527305cf4c
IlyaTrifonov <ilya.trifonov.official@gmail.com>
2023-01-12 01:31:02 +0300

    Added a category icon on the letter.

--- src/client/src/components/Letter/Letter.jsx
+++ src/client/src/components/Letter/Letter.jsx
@@ -6,6 +6,8 @@ import MailService from "../../api/MailService";
 import ReadMark from "../LetterItem/ReadMark";
 import UserAvatar from "../LetterItem/UserAvatar";
 import {getDateString} from "../dateParse";
+import {flags} from "../LetterItem/flags";
+import Icons from "../../assets/icons/Icons";
 
 const Letter = ({/*folder*/}) => {
     // const [selectedFolder, setSelectedFolder] = useState(folder)
@@ -50,7 +52,24 @@ const Letter = ({/*folder*/}) => {
         letter && <div className="letter">
             <div className="letter__header">
                 <h2 className="letter__tittle">{letter.title}</h2>
-                <div className="letter__flag">{letter.flag || 'Письмо без категории'}</div>
+                {letter.flag ?
+                    <div className="letter__flag">
+                        <Icons name={flags[letter.flag]}
+                               width="16"
+                               height="16"
+                               className="secondary-data__flag-icon"/>
+                        <div className="letter__flag__text">
+                            {letter.flag}
+                        </div>
+                    </div>
+                    :
+                    <div className="letter__flag">
+                        <div className="letter__flag__text">
+                            Письмо без категории
+                        </div>
+                    </div>
+                }
+
             </div>
             <div className="letter__head">
                 <ReadMark isRead={letter.read}/>
