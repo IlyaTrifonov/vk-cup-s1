cb0081b06e7c80cff76d4288a511b2c2368856e1
IlyaTrifonov <ilya.trifonov.official@gmail.com>
2023-01-19 17:48:56 +0300

    Added dependence of navbar components, depending on the opening of the letter.

--- src/client/src/components/navbar/Navbar.jsx
+++ src/client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import './Navbar.sass'
 import {ReactComponent as Logo} from "../../assets/svg/logo.svg";
 import FilterButton from "./FilterButton";
+import {useMatch, useNavigate} from "react-router-dom";
+import Icons from "../../assets/icons/Icons";
+import {uiIcons} from "../../assets/icons";
+import {LanguageContext} from "../../context/LanguageContext";
 
 /**
  * Компонент навбара. Отображает навбар :)
@@ -9,13 +13,35 @@ import FilterButton from "./FilterButton";
  * @constructor
  */
 const Navbar = () => {
+
+    const {language} = useContext(LanguageContext);
+    const match = useMatch('/letter/:id');
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        console.log('Мы в письме:', match)
+    }, [match])
+
     return (
         <div className="navbar">
             <div className="navbar-container">
-                <a className="navbar__logo" href="/">
-                    <Logo/>
-                </a>
-                <FilterButton/>
+                {match ?
+                    <div className="go-back-button"
+                         onClick={() => navigate(-1)}>
+                        <Icons name={uiIcons.chevron_left}
+                               width="20"
+                               height="20"
+                               className="go-back-button__icon"/>
+                        <span className="go-back-button__text">
+                            {language.letterPage.goBackButtonName}
+                        </span>
+                    </div>
+                    :
+                    <div className="navbar__logo" onClick={() => navigate('/')}>
+                        <Logo/>
+                    </div>
+                }
+                {!match && <FilterButton/>}
             </div>
         </div>
     );
